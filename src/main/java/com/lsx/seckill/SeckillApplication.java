f5156c84d59062043c6db72db552198d838dffae
LiSongXi <18813343309@163.com>
2022-05-10 14:01:31 +0800

    2022.5.10

--- src/main/java/com/lsx/seckill/SeckillApplication.java
+++ src/main/java/com/lsx/seckill/SeckillApplication.java
@@ -1,9 +1,11 @@
 package com.lsx.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lsx.seckill.pojo")
 public class SeckillApplication {
 
     public static void main(String[] args) {
