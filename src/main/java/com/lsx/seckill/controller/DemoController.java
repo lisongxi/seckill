3c6c24e2bd9c577eddb723031674897f4dbdcf0c
LiSongXi <18813343309@163.com>
2022-05-10 14:01:51 +0800

    2022.5.10

---
+++ src/main/java/com/lsx/seckill/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.lsx.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 测试
+ *
+ * @author LiSongXi
+ * @since 1.0.0
+ * */
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name","lsx");
+        return "hello";
+    }
+}
